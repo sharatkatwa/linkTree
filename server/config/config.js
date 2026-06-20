673519b93e52d1f33f073b7891381817b303467c
sharatkatwa <sjkatwa2000@gmail.com>
2026-06-20 19:54:12 +0530

    fix: nodeenv error

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ const env = {
     PORT:process.env.PORT || 3000,
     MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/linktree",
     JWT_SECRET: process.env.JWT_SECRET,
-    CORS_ORIGIN: process.env.CORS_ORIGIN
+    CORS_ORIGIN: process.env.CORS_ORIGIN,
     NODE_ENV: process.env.NODE_ENV
 }
 
